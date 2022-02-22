aaddfa725458c73c317c1deb425b6c6325afdf3f
Devanatha R <dev92r@gmail.com>
2022-02-22 22:26:12 +0530

    Testing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let heading = document.getElementById('main-heading');
 let subheading = document.getElementById('main-sub-heading');
 
-
-
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
     heading.style.left = value * 2 + 'px';
